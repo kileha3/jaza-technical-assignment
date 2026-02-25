61a72465a40c76daa41aeb1dfa37d2ea05066715
Lukundo Kileha <lkileha@gmail.com>
2026-02-25 12:28:13 +0300

    refactor: Cleaned up and updated Main activity and Manifest

--- app/src/main/java/jaza/technical/assessment/MainActivity.kt
+++ app/src/main/java/jaza/technical/assessment/MainActivity.kt
@@ -3,45 +3,35 @@ package jaza.technical.assessment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import jaza.technical.assessment.ui.theme.GitHubUsersTheme
+import dagger.hilt.android.AndroidEntryPoint
+import jaza.technical.assessment.ui.theme.GitHubUserBrowserTheme
+import jaza.technical.assessment.ui.theme.ThemeViewModel
+import kotlin.getValue
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val themeViewModel: ThemeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            GitHubUsersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val isDarkTheme by themeViewModel.isDarkTheme.collectAsState()
+            GitHubUserBrowserTheme(darkTheme = isDarkTheme) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GitHubUsersTheme {
-        Greeting("Android")
     }
 }
